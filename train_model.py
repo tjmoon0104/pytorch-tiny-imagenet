d73c3ecbfe52a0b389daf37b2eec2cf11e9ef3b4
Taejun Moon <taejun.moon@swissmarketplace.group>
2023-12-01 15:33:20 +0100

    change train to use other device

--- train_model.py
+++ train_model.py
@@ -1,92 +1,100 @@
-import torch, time, copy, sys, os
-import matplotlib.pyplot as plt
+import copy
+import os
+import sys
+import time
+import torch
 from livelossplot import PlotLosses
 
-def train_model(output_path, model, dataloaders, dataset_sizes, criterion, optimizer, num_epochs=5, scheduler=None):
-    if not os.path.exists('models/'+str(output_path)):
-        os.makedirs('models/'+str(output_path))
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+def train_model(output_path, model, dataloaders, criterion, optimizer, num_epochs=5, scheduler=None):
+    if not os.path.exists("models/" + str(output_path)):
+        os.makedirs("models/" + str(output_path))
+    if torch.cuda.is_available():
+        device = "cuda:0"
+    elif torch.backends.mps.is_built():
+        device = torch.device("mps")
+    else:
+        device = "cpu"
+    # device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     since = time.time()
     liveloss = PlotLosses()
     best_model_wts = copy.deepcopy(model.state_dict())
     best_acc = 0.0
     best = 0
+
     for epoch in range(num_epochs):
-        print('Epoch {}/{}'.format(epoch+1, num_epochs))
-        print('-' * 10)
+        logs = {}
+        print("Epoch {}/{}".format(epoch + 1, num_epochs))
+        print("-" * 10)
 
         # Each epoch has a training and validation phase
-        for phase in ['train', 'val']:
-            if phase == 'train':
+        for phase in ["train", "val"]:
+            if phase == "train":
                 if scheduler != None:
                     scheduler.step()
                 model.train()  # Set model to training mode
             else:
-                model.eval()   # Set model to evaluate mode
+                model.eval()  # Set model to evaluate mode
 
             running_loss = 0.0
             running_corrects = 0
 
             # Iterate over data.
-            for i,(inputs, labels) in enumerate(dataloaders[phase]):
+            for i, (inputs, labels) in enumerate(dataloaders[phase]):
                 inputs = inputs.to(device)
                 labels = labels.to(device)
 
-                # zero the parameter gradients
-                optimizer.zero_grad()
+                outputs = model(inputs)
+                loss = criterion(outputs, labels)
 
-                # forward
-                # track history if only in train
-                with torch.set_grad_enabled(phase == 'train'):
-                    outputs = model(inputs)
-                    _, preds = torch.max(outputs, 1)
-                    loss = criterion(outputs, labels)
-
-                    # backward + optimize only if in training phase
-                    if phase == 'train':
-                        loss.backward()
-                        optimizer.step()
+                if phase == "train":
+                    optimizer.zero_grad()
+                    loss.backward()
+                    optimizer.step()
 
                 # statistics
-                running_loss += loss.item() * inputs.size(0)
+                _, preds = torch.max(outputs, 1)
+                running_loss += loss.detach() * inputs.size(0)
                 running_corrects += torch.sum(preds == labels.data)
-                print("\rIteration: {}/{}, Loss: {}.".format(i+1, len(dataloaders[phase]), loss.item() * inputs.size(0)), end="")
 
-#                 print( (i+1)*100. / len(dataloaders[phase]), "% Complete" )
+                print(
+                    "\rIteration: {}/{}, Loss: {}.".format(
+                        i + 1, len(dataloaders[phase]), loss.item() * inputs.size(0)
+                    ),
+                    end="",
+                )
+
+                #                 print( (i+1)*100. / len(dataloaders[phase]), "% Complete" )
                 sys.stdout.flush()
-                
-                
-            epoch_loss = running_loss / dataset_sizes[phase]
-            epoch_acc = running_corrects.double() / dataset_sizes[phase]
-            if phase == 'train':
-                avg_loss = epoch_loss
-                t_acc = epoch_acc
+
+            epoch_loss = running_loss / len(dataloaders[phase].dataset)
+            epoch_acc = running_corrects.float() / len(dataloaders[phase].dataset)
+
+            if phase == "train":
+                prefix = ''
+                # avg_loss = epoch_loss
+                # t_acc = epoch_acc
             else:
-                val_loss = epoch_loss
-                val_acc = epoch_acc
-            
-#             print('{} Loss: {:.4f} Acc: {:.4f}'.format(
-#                 phase, epoch_loss, epoch_acc))
+                prefix = 'val_'
+                # val_loss = epoch_loss
+                # val_acc = epoch_acc
+
+            #             print('{} Loss: {:.4f} Acc: {:.4f}'.format(
+            #                 phase, epoch_loss, epoch_acc))
 
             # deep copy the model
-            if phase == 'val' and epoch_acc > best_acc:
+            if phase == "val" and epoch_acc > best_acc:
                 best_acc = epoch_acc
                 best = epoch + 1
                 best_model_wts = copy.deepcopy(model.state_dict())
-                
-        liveloss.update({
-            'log loss': avg_loss,
-            'val_log loss': val_loss,
-            'accuracy': t_acc,
-            'val_accuracy': val_acc
-        })
-                
-        liveloss.draw()
-        print('Train Loss: {:.4f} Acc: {:.4f}'.format(avg_loss, t_acc))
-        print(  'Val Loss: {:.4f} Acc: {:.4f}'.format(val_loss, val_acc))
-        print()
-        torch.save(model.state_dict(), './models/' + str(output_path) + '/model_{}_epoch.pt'.format(epoch+1))
+
+            logs[prefix + 'log loss'] = epoch_loss.item()
+            logs[prefix + 'accuracy'] = epoch_acc.item()
+
+        liveloss.update(logs)
+        liveloss.send()
+
+        torch.save(model.state_dict(), f"./models/{output_path}/model_{epoch + 1}_epoch.pt")
     time_elapsed = time.time() - since
-    print('Training complete in {:.0f}m {:.0f}s'.format(
-        time_elapsed // 60, time_elapsed % 60))
-    print('Best Validation Accuracy: {}, Epoch: {}'.format(best_acc, best))
+    print("Training complete in {:.0f}m {:.0f}s".format(time_elapsed // 60, time_elapsed % 60))
+    print("Best Validation Accuracy: {}, Epoch: {}".format(best_acc, best))
